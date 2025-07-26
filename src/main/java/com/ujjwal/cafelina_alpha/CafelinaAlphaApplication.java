84561c4dc78a080db8cbe9f4f9f5ac9003f0237d
Ujjwal-Agarwal <ujjwal7648@outlook.com>
2025-07-27 03:43:26 +0530

    Initial Commit

---
+++ src/main/java/com/ujjwal/cafelina_alpha/CafelinaAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.ujjwal.cafelina_alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafelinaAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafelinaAlphaApplication.class, args);
+	}
+
+}
